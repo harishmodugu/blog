63ca10cc756e38099cf2140d2a646a9f52c100a2
Harish Modugu <harish.modugu@gmail.com>
2018-05-09 11:10:17 -0600

    My first post

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Harish Modugu`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,10 +27,9 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
+          software developer and husband. {' '} <br/>
+          <a href="https://twitter.com/harishmodugu">
+           Follow me on twitter
           </a>
         </p>
       </div>
---
+++ src/pages/first-post/index.md
@@ -0,0 +1,12 @@
+---
+title: My first post
+date: "2018-04-09"
+---
+
+I have been wanting to write for a long time and here it is my first post.
+
+I am planning on writing mostly about software development and any other interesting things I learn or do.
+
+I believe writing helps me think clearly and articulate my thoughts better. Looking forward to share my learnings and thoughts.
+
+Stay tuned!
